6bf1651bf8f75d825a4813e0e1245dce77cc7049
pochiel <dj_pochiel@hotmail.com>
2022-03-07 17:59:24 +0900

    bug fix

--- t_token.hpp
+++ t_token.hpp
@@ -1,33 +1,15 @@
 #ifndef __T_TOKEN_HPP_
 #define __T_TOKEN_HPP_
+#include <string>
 class t_token {
 	public:
-	t_token(){
-		token_str = "";
-		comment = "";
-	}
-	t_token(const t_token &t){
-		token_str = t.token_str;
-		comment = t.comment;
-	}
+	t_token();
+	t_token(const t_token &t);
 	std::string token_str;
 	std::string comment;
-	std::string get_format_comment() {
-		if(comment.empty()){
-			return "";
-		} else {
-			return "note right\n"
-					+ comment + "\n"
-					+ "end note\n";
-		}
-	}
+	std::string get_format_comment();
 	//  +演算子のオーバーロード
-	t_token operator+(const t_token& t2) {
-		t_token ret;
-		ret.token_str = this->token_str + t2.token_str;
-		ret.comment = this->comment + t2.comment;
-		return ret;
-	}
+	t_token operator+(const t_token& t2);
 };
 
 #endif // __T_TOKEN_HPP_
